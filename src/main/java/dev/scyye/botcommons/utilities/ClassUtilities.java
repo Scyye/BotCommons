79f4852dcdf68cdc3f08c9e88126817489ebfdfe
AzraelHerself <scyyebiz@gmail.com>
2024-05-25 22:09:59 -0400

    Deprecate Utility classes

--- src/main/java/dev/scyye/botcommons/utilities/ClassUtilities.java
+++ src/main/java/dev/scyye/botcommons/utilities/ClassUtilities.java
@@ -2,6 +2,7 @@ package dev.scyye.botcommons.utilities;
 
 import java.lang.reflect.Field;
 
+@Deprecated(since = "1.7", forRemoval = true)
 public class ClassUtilities {
 	public static <T> T getFieldOfType(Object object, Class<T> type) {
 		for (Field field : object.getClass().getDeclaredFields()) {
--- src/main/java/dev/scyye/botcommons/utilities/StringUtilities.java
+++ src/main/java/dev/scyye/botcommons/utilities/StringUtilities.java
@@ -1,5 +1,6 @@
 package dev.scyye.botcommons.utilities;
 
+@Deprecated(since = "1.7", forRemoval = true)
 public class StringUtilities {
 	public static String replaceSpecificCharacter(String string, char replacement, int index) {
 		char[] chars = string.toCharArray();
