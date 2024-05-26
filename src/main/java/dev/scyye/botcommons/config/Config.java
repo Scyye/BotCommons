3d381dc0c5bf428221444274272a745650363e0b
AzraelHerself <scyyebiz@gmail.com>
2024-05-25 22:12:43 -0400

    Standardize Config with better handlers

--- src/main/java/dev/scyye/botcommons/config/Config.java
+++ src/main/java/dev/scyye/botcommons/config/Config.java
@@ -2,6 +2,7 @@ package dev.scyye.botcommons.config;
 
 import com.google.gson.GsonBuilder;
 import dev.scyye.botcommons.utilities.ClassUtilities;
+import lombok.Getter;
 import net.dv8tion.jda.api.JDA;
 
 import java.io.*;
@@ -10,38 +11,53 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class Config extends HashMap<String, Object> {
-	public static String botName;
-	public static String ownerId;
+	@Getter
+	private static Config instance;
 
 	/**
 	 * Creates a config file with the given values and bot name
-	 * @param values The values to put in the config
+	 * @param oldValues The values to put in the config
 	 * @return The {@link Config} object
 	 */
-	public static Config makeConfig(Map<String, Object> values) {
+	public static Config makeConfig(Map<String, Object> oldValues, String botName) {
+		HashMap<String, String> values = new HashMap<>();
+		values.put("bot-name", botName);
+		values.put("token", "TOKEN");
 		String fileName = botName + "-assets\\config.json";
 		if (Files.exists(Path.of(fileName))) {
 			try {
-				return makeConfig(fileName);
+				instance = makeConfig(fileName);
+				return instance;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		new File(botName+"-assets").mkdirs();
 		Config config = new Config();
+		oldValues.forEach((key, value) -> {
+			if (value instanceof String)
+				values.put(key, (String) value);
+			else
+				values.put(key, new GsonBuilder().setPrettyPrinting().create().toJson(value));
+		});
 		config.putAll(values);
-		Config.botName = botName;
 		try {
-			config.write(botName);
+			config.write();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return config;
+		instance = config;
+		return instance;
 	}
 
 	public static Config makeConfig(String file) throws IOException {
-		return new GsonBuilder().setPrettyPrinting().create()
+		Config config = new GsonBuilder().setPrettyPrinting().create()
 				.fromJson(Files.readString(new File(file).toPath()), Config.class);
+
+		if (config.get("token").equalsIgnoreCase("TOKEN"))
+			throw new IllegalArgumentException("Token not set in config file");
+
+		return config;
 	}
 
 	@Override
@@ -67,7 +83,7 @@ public class Config extends HashMap<String, Object> {
 		return get(key.toString(), String.class);
 	}
 
-	public void write(String botName) throws IOException {
-		Files.writeString(Path.of(botName + "-assets", "config.json"), toString());
+	public void write() throws IOException {
+		Files.writeString(Path.of(Config.instance.get("bot-name") + "-assets", "config.json"), toString());
 	}
 }
